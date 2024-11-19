0bb00bd01bf006cb95a796d6be804a9497c0723a
Juan Estay Rodriguez <juan.estay.rodriguez@icloud.com>
2024-11-19 13:21:37 -0300

    Proyectos destacados V1

--- src/assets/components/CustomCard.jsx
+++ src/assets/components/CustomCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';  // Importa Link para navegación entre rutas de React
 
-function CustomCard({ title, image, description }) {
+function CustomCard({ title, image, description, projectId }) {
   return (
     <Card className="custom-card">
       <Card.Img variant="top" src={image} className="card-image" />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
+        {/* Enlace para abrir el detalle en una nueva pestaña */}
+        <a 
+          href={`/proyecto/${projectId}`} 
+          target="_blank"  // Esto indica que debe abrirse en una nueva pestaña
+          rel="noopener noreferrer" // Seguridad adicional al usar target="_blank"
+          className="boton-p-p">
+          Ver detalles
+        </a>
       </Card.Body>
     </Card>
   );
