4e42fa8284e5cba0fd91506975ddac8640c31c82
Juanith0xx <juan.estay.rodriguez@icloud.com>
2024-11-19 22:04:44 -0300

    Mail configurado y footer redes sociales

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
       <p className="footer-p">Correo Electronico: <b>contacto@sistemamuebles.cl</b></p>
     </div>
     <div className="footer-social">
-      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+      <a href="#" target="_blank" rel="noopener noreferrer">
       <i className="fab fa-facebook"></i>
       </a>
-      <a href="https://web.whatsapp.com/" target="_blank" rel="noopener noreferrer">
+      <a href="#" target="_blank" rel="noopener noreferrer">
       <i className="fab fa-whatsapp"></i>
       </a>
-      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+      <a href="#" target="_blank" rel="noopener noreferrer">
       <i className="fab fa-instagram"></i>
       </a>
     </div>
