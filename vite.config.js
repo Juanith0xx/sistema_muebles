9f7c6201aff5aaf0fe9d0dfd31dac449483d4f60
Juan Estay Roriguez <juan.estay.rodriguez@icloud.com>
2025-01-08 22:16:05 -0300

    web-desplegada v2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: "", // Ruta base para GitHub Pages
-  plugins: [react()],
+  base: './', // Asegura rutas relativas para cPanel
+  plugins: [react()]
 });
