2dc1ebe8da550554d6d73003d04118cc0a5ac14b
Juan Estay R <juan.estay.rodriguez@icloud.com>
2025-04-09 16:59:39 -0400

    revision de responsivo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,7 @@ const Footer = () => {
       <h5 className="footer-h5">Sistema Muebles.cl</h5>
       <p className="footer-p">Correo Electronico: <b>contacto@sistemamuebles.cl</b></p>
     </div>
+    {/*
     <div className="footer-social">
       <a href="#" target="_blank" rel="noopener noreferrer">
       <i className="fab fa-facebook"></i>
@@ -18,6 +19,7 @@ const Footer = () => {
       <i className="fab fa-instagram"></i>
       </a>
     </div>
+    */}
     <div className="footer-copyright">
       <p>© 2024 Sistema Muebles. Todos los derechos reservados.</p>
     </div>
